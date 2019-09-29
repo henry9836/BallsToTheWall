212e79bf1df8007a04cc1ffecbb76bdae7f0da9f
Henry Oliver <37818378+henry9836@users.noreply.github.com>
2019-09-30 09:59:35 +1300

    Add files via upload

--- SwiftlyBallsToTheWall/GameScene.swift
+++ SwiftlyBallsToTheWall/GameScene.swift
@@ -11,29 +11,91 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var mainMenuTitle: SKLabelNode!
+    var logo: SKShapeNode!
     
-    override func didMove(to view: SKView) {
+    let firstNode = SKNode()
+    let nonTexturedSpriteNodeFirst = SKSpriteNode()
+    let nonTexturedSpriteNodeSecond = SKSpriteNode()
+    let texturedSpriteNode = SKSpriteNode(imageNamed: "wow")
+    
+    override func didMove(to view: SKView){
+        createText()
+        createShapes()
+        nonTexturedSpriteNodeSecond.addChild(texturedSpriteNode)
+        nonTexturedSpriteNodeFirst.addChild(nonTexturedSpriteNodeSecond)
+        firstNode.addChild(nonTexturedSpriteNodeFirst)
+        addChild(firstNode)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        firstNode.position = CGPoint(x: self.frame.midX, y:self.frame.midY)
+        nonTexturedSKSpriteNodes()
+        texturedSKSpriteNodes()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let location = touches.first?.location(in: self){
+            if texturedSpriteNode.contains(location){
+                texturedSpriteNode.size = CGSize(width: texturedSpriteNode.size.width * 2, height: texturedSpriteNode.size.height * 2)
+            }
         }
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
+        if let location = touches.first?.location(in: self){
+            if texturedSpriteNode.contains(position){
+                texturedSpriteNode.position = location
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?){
+        if let location = touches.first?.location(in :self){
+            if texturedSpriteNode.contains(location){
+                texturedSpriteNode.size = CGSize(width: texturedSpriteNode.size.width * 0.5, height: texturedSpriteNode.size.height * 0.5)
+            }
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch cancelled")
+    }
+    
+    func createShapes(){
+        logo = SKShapeNode(rectOf: CGSize(width: 100, height:100))
+        logo.fillColor = SKColor.red
+        logo.fillTexture = SKTexture(imageNamed: "logo")
+        logo.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - 300.0)
+        self.addChild(logo)
+        
+    }
+    
+    func createText(){
+        mainMenuTitle = SKLabelNode()
+        mainMenuTitle.text = "Balls To The Wall"
+        mainMenuTitle.fontSize = 32.0
+        //mainMenuTitle.fontName = "AvenirNext-Bold"
+        mainMenuTitle.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - 100.0)
+        mainMenuTitle.fontColor = UIColor.white
+        self.addChild(mainMenuTitle)
+    }
+    
+    func texturedSKSpriteNodes(){
+        texturedSpriteNode.size = CGSize(width: 64, height: 64)
+        texturedSpriteNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    }
+    
+    func nonTexturedSKSpriteNodes(){
+        nonTexturedSpriteNodeFirst.name = "first"
+        nonTexturedSpriteNodeFirst.color = UIColor.blue
+        nonTexturedSpriteNodeFirst.size = CGSize(width: 128, height: 128)
+        nonTexturedSpriteNodeFirst.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        nonTexturedSpriteNodeSecond.name = "secodn"
+        nonTexturedSpriteNodeSecond.color = UIColor.yellow
+        nonTexturedSpriteNodeSecond.size = CGSize(width: 100, height:  100)
+        nonTexturedSpriteNodeSecond.anchorPoint = CGPoint(x: 0.5, y:0.5)
+        nonTexturedSpriteNodeSecond.position = CGPoint(x: 0, y: 0)
+    }
+    
 }
--- SwiftlyBallsToTheWall/GameViewController.swift
+++ SwiftlyBallsToTheWall/GameViewController.swift
@@ -1,35 +1,27 @@
-//
-//  GameViewController.swift
-//  SwiftlyBallsToTheWall
-//
-//  Created by Henry Oliver on 30/09/19.
-//  Copyright © 2019 Henry Oliver. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: view.bounds.size)
             
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
             view.showsPhysics = true
         }
     }
+    
+    
 }
