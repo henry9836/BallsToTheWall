6df1edba5b25129927e4452cbaa1b5a114da9c70
Henry Oliver <37818378+henry9836@users.noreply.github.com>
2019-10-01 11:13:12 +1300

    Full gameplay loop

--- SwiftlyBallsToTheWall/GameScene.swift
+++ SwiftlyBallsToTheWall/GameScene.swift
@@ -13,78 +13,169 @@ class GameScene: SKScene {
 
     
     
-    let firstNode = SKNode()
-    let nonTexturedSpriteNodeFirst = SKSpriteNode()
-    let nonTexturedSpriteNodeSecond = SKSpriteNode()
-    let texturedSpriteNode = SKSpriteNode(imageNamed: "wow")
+    var gameBall: SKSpriteNode!
+    var wheel: SKSpriteNode!
+    
+    var score: SKLabelNode!
+    var currentScore: Int = 0
+    var fallSpeed: Double = 10.0
+    var fadeLock: Bool = false
+    var inLock: Bool = false
+    var colorMatched: Bool = false
+    var gameOver: Bool = false
+    
+    //0 - red, 1 - yellow, 2 - green, 3 - blue
+    var rotTrack: Int = 0
+    var colorTrack: Int = 0
+    
+    let speedIncrease: Double = 0.9
+    
+    let rotateRightAction = SKAction.rotate(byAngle: CGFloat((Float.pi * 90)/180), duration: 0.3)
+    let rotateLeftAction = SKAction.rotate(byAngle: CGFloat((Float.pi * -90)/180), duration: 0.3)
+    
+    let turnRed = SKAction.colorize(with: UIColor.red, colorBlendFactor: CGFloat(1), duration: 0.3)
+    let turnYellow = SKAction.colorize(with: UIColor.yellow, colorBlendFactor: CGFloat(1), duration: 0.3)
+    let turnBlue = SKAction.colorize(with: UIColor.blue, colorBlendFactor: CGFloat(1), duration: 0.3)
+    let turnGreen = SKAction.colorize(with: UIColor.green, colorBlendFactor: CGFloat(1), duration: 0.3)
+    let ballFadeOut = SKAction.fadeAlpha(by: -1.0, duration: 0.3)
+    let ballFadeIn = SKAction.fadeAlpha(by: 1.0, duration: 0.3)
+    
     
     override func didMove(to view: SKView){
         createText()
-        createShapes()
-        //nonTexturedSpriteNodeSecond.addChild(texturedSpriteNode)
-        nonTexturedSpriteNodeFirst.addChild(nonTexturedSpriteNodeSecond)
-        firstNode.addChild(nonTexturedSpriteNodeFirst)
-        self.addChild(firstNode)
-        self.addChild(texturedSpriteNode)
+        createSprites()
         
-        firstNode.position = CGPoint(x: self.frame.midX, y:self.frame.midY)
-        nonTexturedSKSpriteNodes()
-        texturedSKSpriteNodes()
-        
+        gameStart()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let location = touches.first?.location(in: self){
-            if texturedSpriteNode.contains(location){
-                texturedSpriteNode.size = CGSize(width: texturedSpriteNode.size.width * 2, height: texturedSpriteNode.size.height * 2)
-            }
+        if (gameOver){
+            let newScene = MainMenuScene(size: (self.view?.bounds.size)!)
+            let transition = SKTransition.reveal(with: .up, duration: 2)
+            self.view?.presentScene(newScene, transition: transition)
+            transition.pausesOutgoingScene = true
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
-        if let location = touches.first?.location(in: self){
-            if texturedSpriteNode.contains(position){
-                texturedSpriteNode.position = location
-            }
-        }
+
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?){
-        if let location = touches.first?.location(in :self){
-            if texturedSpriteNode.contains(location){
-                texturedSpriteNode.size = CGSize(width: texturedSpriteNode.size.width * 0.5, height: texturedSpriteNode.size.height * 0.5)
+        if let location =  touches.first?.location(in: self.view){
+            if (location.x < self.frame.midX){ //user tapped on left side of screen
+                wheel.run(rotateRightAction)
+                trackRotation(dir: 1)
             }
+            else{ //user tapped on right side of screen
+                wheel.run(rotateLeftAction)
+                trackRotation(dir: -1)
+            }
+            
         }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touch cancelled")
-    }
-    
-    func createShapes(){
 
     }
     
-    func createText(){
- 
-    }
-    
-    func texturedSKSpriteNodes(){
-        texturedSpriteNode.size = CGSize(width: 64, height: 64)
-        texturedSpriteNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-    }
-    
-    func nonTexturedSKSpriteNodes(){
-        nonTexturedSpriteNodeFirst.name = "first"
-        nonTexturedSpriteNodeFirst.color = UIColor.blue
-        nonTexturedSpriteNodeFirst.size = CGSize(width: 128, height: 128)
-        nonTexturedSpriteNodeFirst.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    override func update(_ currentTime: TimeInterval) {
         
-        nonTexturedSpriteNodeSecond.name = "secodn"
-        nonTexturedSpriteNodeSecond.color = UIColor.yellow
-        nonTexturedSpriteNodeSecond.size = CGSize(width: 100, height:  100)
-        nonTexturedSpriteNodeSecond.anchorPoint = CGPoint(x: 0.5, y:0.5)
-        nonTexturedSpriteNodeSecond.position = CGPoint(x: 0, y: 0)
+        if (gameBall.position.y == (self.frame.minY + 150)){
+            if (!fadeLock){
+                fadeLock = true
+                gameBall.run(ballFadeOut)
+                pickBallColor()
+                fallSpeed = fallSpeed * speedIncrease
+            }
+            if (!gameBall.hasActions()){
+                if (colorMatched){
+                    gameBall.position.y = self.frame.maxY - 200
+                }
+            }
+        }
+        else if (gameBall.position.y < 250){
+            if (!inLock){
+                if (rotTrack == colorTrack){
+                    colorMatched = true
+                    currentScore = currentScore + 1
+                    score.text = String(currentScore)
+                }
+                else{
+                    UserDefaults.standard.set(currentScore, forKey: "roundScore")
+                    score.text = "Gameover\nScore: " + String(currentScore)
+                    if (UserDefaults.standard.integer(forKey: "highScore") < currentScore){
+                        UserDefaults.standard.set(currentScore, forKey: "highScore")
+                        score.text = "Gameover\nNew HighScore!\nScore: " + String(currentScore)
+                    }
+                    gameOver = true
+                    colorMatched = false
+                }
+                inLock = true
+                
+            }
+        }
+        else{
+            inLock = false
+            if (fadeLock){
+                gameBall.run(ballFadeIn)
+                gameBall.run(SKAction.moveTo(y: CGFloat(self.frame.minY + 150), duration: fallSpeed))
+                fadeLock = false
+            }
+        }
+    }
+    
+    func trackRotation(dir: Int){
+        rotTrack = rotTrack + dir
+        if (rotTrack < 0){
+            rotTrack = 3
+        }
+        else if (rotTrack > 3){
+            rotTrack = 0
+        }
+    }
+    
+    func pickBallColor(){
+        let number = Int.random(in: 0 ... 3)
+        if (number == 0){
+            gameBall.run(turnRed)
+        }
+        else if (number == 1){
+            gameBall.run(turnYellow)
+        }
+        else if (number == 2){
+            gameBall.run(turnGreen)
+        }
+        else{
+            gameBall.run(turnBlue)
+        }
+        colorTrack = number
+    }
+    
+    func gameStart(){
+        gameBall.run(SKAction.moveTo(y: CGFloat(self.frame.minY + 150), duration: fallSpeed))
+        pickBallColor()
+        score.numberOfLines = 0
+    }
+    
+    func createSprites(){
+        wheel = SKSpriteNode(texture: SKTexture(imageNamed: "circle"), size: CGSize(width: 250, height: 250))
+        wheel.position = CGPoint(x: self.frame.midX, y: self.frame.minY + 150)
+        self.addChild(wheel)
+        
+        gameBall = SKSpriteNode(texture: SKTexture(imageNamed: "ball"), size: CGSize(width: 50, height: 50))
+        gameBall.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - 200)
+        self.addChild(gameBall)
+    }
+    
+    func createText(){
+        score = SKLabelNode()
+        score.text = "0"
+        score.fontSize = 32.0
+        //mainMenuTitle.fontName = "AvenirNext-Bold"
+        score.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        score.fontColor = UIColor.white
+        self.addChild(score)
     }
     
 }
--- SwiftlyBallsToTheWall/MainMenuScene.swift
+++ SwiftlyBallsToTheWall/MainMenuScene.swift
@@ -11,50 +11,34 @@ import GameplayKit
 
 class MainMenuScene: SKScene {
     
-    let left = SKSpriteNode()
-    let right = SKSpriteNode()
     
     var logo: SKSpriteNode!
     var mainMenuTitle: SKLabelNode!
+    var tapToPlay: SKLabelNode!
+    var highScoreText: SKLabelNode!
+    
     
     override func didMove(to view: SKView){
         //self.backgroundColor = UIColor.gray
         createText()
         createShapes()
-        
-        
-        left.color = UIColor.red
-        left.size = CGSize(width: 64, height: 64)
-        left.position = CGPoint(x: self.frame.width/2 - 64, y: self.frame.height/2)
-        addChild(left)
-        
-        right.color = UIColor.blue
-        right.size = CGSize(width: 64, height: 64)
-        right.position = CGPoint(x: self.frame.width/2 + 64, y: self.frame.height/2)
-        addChild(right)
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let location = touches.first?.location(in: self)
-        if left.contains(location!){
-            let newScene = GameScene(size: (self.view?.bounds.size)!)
-            let transition = SKTransition.reveal(with: .up, duration: 2)
-            self.view?.presentScene(newScene, transition: transition)
-            transition.pausesOutgoingScene = true
-            transition.pausesOutgoingScene = false
-        }
-        else if right.contains(location!){
-            let newScene = GameScene(size: (self.view?.bounds.size)!)
-            let transition = SKTransition.reveal(with: .up, duration: 2)
-            self.view?.presentScene(newScene, transition: transition)
-            transition.pausesOutgoingScene = true
-            transition.pausesOutgoingScene = true
+        let newScene = GameScene(size: (self.view?.bounds.size)!)
+        let transition = SKTransition.reveal(with: .up, duration: 2)
+        self.view?.presentScene(newScene, transition: transition)
+        transition.pausesOutgoingScene = true
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if (!logo.hasActions()){
+            logo.run(SKAction.rotate(byAngle: -1/5, duration: 1))
         }
     }
     
     func createShapes(){
-        logo = SKSpriteNode(texture: SKTexture(imageNamed: "logo"), size: CGSize(width: 100, height:100))
+        logo = SKSpriteNode(texture: SKTexture(imageNamed: "logo"), size: CGSize(width: 800, height: 800))
         logo.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         self.addChild(logo)
         
@@ -65,10 +49,27 @@ class MainMenuScene: SKScene {
         mainMenuTitle.text = "Balls To The Wall"
         mainMenuTitle.fontSize = 32.0
         //mainMenuTitle.fontName = "AvenirNext-Bold"
-        mainMenuTitle.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - 100.0)
+        mainMenuTitle.position = CGPoint(x: self.frame.midX, y: self.frame.midY+100)
+        mainMenuTitle.fontColor = UIColor.white
+        self.addChild(mainMenuTitle)
+        
+        mainMenuTitle = SKLabelNode()
+        mainMenuTitle.text = "Tap To Start"
+        mainMenuTitle.fontSize = 15.0
+        //mainMenuTitle.fontName = "AvenirNext-Bold"
+        mainMenuTitle.position = CGPoint(x: self.frame.midX, y: self.frame.midY-50)
+        mainMenuTitle.fontColor = UIColor.white
+        self.addChild(mainMenuTitle)
+        
+        mainMenuTitle = SKLabelNode()
+        mainMenuTitle.text = "HighScore: " + String(UserDefaults.standard.integer(forKey: "highScore"))
+        mainMenuTitle.fontSize = 32.0
+        //mainMenuTitle.fontName = "AvenirNext-Bold"
+        mainMenuTitle.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         mainMenuTitle.fontColor = UIColor.white
         self.addChild(mainMenuTitle)
     }
+
     
     
     
